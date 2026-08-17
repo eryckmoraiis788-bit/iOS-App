b6fbd96953c1a4ea40ff09b57ed4a48b16cbd0a4
eryckmoraiis788-bit <eryckmoraiis788@gmail.com>
2026-08-17 23:06:34 +0000

    Inter 1.2.7: corrigir confirmação de envio

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.6",
+  version: "1.2.7",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "126",
+    buildNumber: "127",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
