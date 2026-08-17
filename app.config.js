5166e28e3a37a2915154626ecddabcbd7458597b
eryckmoraiis788-bit <eryckmoraiis788@gmail.com>
2026-08-17 14:19:41 -0300

    Inter 1.2.1 layout modelos no Compor

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.0",
+  version: "1.2.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
