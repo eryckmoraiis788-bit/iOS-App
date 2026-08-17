f4c36d3ecef7c74ff5362a9d3cf8dc91d7636cc8
eryckmoraiis788-bit <eryckmoraiis788@gmail.com>
2026-08-17 18:02:48 +0000

    Inter 1.2.3 build 123

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.2",
+  version: "1.2.3",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,6 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
+    buildNumber: "123",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
