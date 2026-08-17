6a07dae9a451de7c1cf6f5f6af6cc6dc5888d66b
eryckmoraiis788-bit <3.16219536e+08+eryckmoraiis788-bit@users.noreply.github.com>
2026-08-17 18:23:21 +0000

    Fix direct template save and reduce header

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.3",
+  version: "1.2.4",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "123",
+    buildNumber: "124",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
