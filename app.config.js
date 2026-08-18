c5d0aabda427ba21a543f1e13b67cd44dd58a22f
eryckmoraiis788-bit <3.16219536e+08+eryckmoraiis788-bit@users.noreply.github.com>
2026-08-18 11:51:12 +0000

    Add Pix presets to scheduled notifications

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.18",
+  version: "1.2.19",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "138",
+    buildNumber: "139",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
