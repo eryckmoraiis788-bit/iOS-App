e95ac643c0ff3546ca5d6ddebc751f7f5027bf34
eryckmoraiis788-bit <eryckmoraiis788@gmail.com>
2026-08-18 19:51:07 +0000

    Inter 1.2.33 build 153: alinhar botão Emitir notificação à pré-visualização

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.32",
+  version: "1.2.33",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "152",
+    buildNumber: "153",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
