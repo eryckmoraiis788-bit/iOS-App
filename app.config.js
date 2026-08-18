2cf2e5d69f682130a139ce7095fc3ba7b1dd0579
eryckmoraiis788-bit <eryckmoraiis788@gmail.com>
2026-08-18 19:26:54 +0000

    fix: show full notification button

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.31",
+  version: "1.2.32",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "151",
+    buildNumber: "152",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
