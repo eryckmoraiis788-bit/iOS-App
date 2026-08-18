af981bbcdec8ee6ca16253094d7f51d1468b76d3
eryckmoraiis788-bit <3.16219536e+08+eryckmoraiis788-bit@users.noreply.github.com>
2026-08-18 01:07:43 +0000

    Fix confirmation modal cancel button layout

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.12",
+  version: "1.2.13",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "132",
+    buildNumber: 133"132",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
