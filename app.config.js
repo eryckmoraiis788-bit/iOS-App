c2b272c8ff7318079d4f5e97ffd3b3143d0f7262
eryckmoraiis788-bit <eryckmoraiis788@gmail.com>
2026-08-18 18:55:33 +0000

    chore: bump Inter to 1.2.31 build 151

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.30",
+  version: "1.2.31",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "150",
+    buildNumber: "151",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
