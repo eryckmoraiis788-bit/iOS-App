48d62017a0d7a6771a6014af9db18671c694ada3
eryckmoraiis788-bit <3.16219536e+08+eryckmoraiis788-bit@users.noreply.github.com>
2026-08-22 18:56:33 +0000

    feat: use dark Inter app logo

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.51",
+  version: "1.2.52",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "171",
+    buildNumber: "172",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
