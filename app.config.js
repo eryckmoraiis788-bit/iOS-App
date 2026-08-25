651a83182834c984c1ab6acab7b79b50e398f0fa
eryckmoraiis788-bit <3.16219536e+08+eryckmoraiis788-bit@users.noreply.github.com>
2026-08-25 19:51:51 +0000

    chore: align Expo SDK 54 patch dependencies

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ const scheme = `manus${timestamp}`;
 module.exports = {
   name: "Inter",
   slug: "notificacao-ios",
-  version: "1.2.62",
+  version: "1.2.63",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme,
@@ -25,7 +25,7 @@ module.exports = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: bundleId,
-    buildNumber: "182",
+    buildNumber: "183",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDisplayName: "Inter",
